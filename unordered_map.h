0f72252a548e558e3141eb6b0776f93bd4b1ea8b
AmnesiaHzd_UCloud <AmnesiaHzd@gmail.com>
2023-10-23 21:17:57 +0800

    hash table

---
+++ unordered_map.h
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace TinySTL {
+
+template <class Key, class Value, class Hash = TinySTL::hash<Key>, class Equal = TinySTL::equal_to<Key>>
+class unordered_map {
+    
+}
+
+} // end namespace TinySTL
